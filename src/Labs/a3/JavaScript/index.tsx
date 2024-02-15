cb4a7264fb8e468bbdfe0979a8a0cc698d7798cb
apurvakhatri <apurvakhatri2011@gmail.com>
2024-02-14 21:39:50 -0500

    Variables and Constants

--- src/Labs/a3/JavaScript/index.tsx
+++ src/Labs/a3/JavaScript/index.tsx
@@ -1,8 +1,11 @@
+import VariablesAndConstants from "./variables/VariablesAndConstants";
+
 function JavaScript() {
     console.log('Hello World!');
     return(
     <div>
         <h1>JavaScript</h1>
+        <VariablesAndConstants />
     </div>
    );
 }
