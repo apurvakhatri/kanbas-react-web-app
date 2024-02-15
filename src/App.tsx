ffb6d616c0386ef3d7e69f21c37186e2940fa385
apurvakhatri <apurvakhatri2011@gmail.com>
2024-02-14 20:29:35 -0500

    Labs added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import Labs from "./Labs";
+
 //import './App.css';
 
 function App() {
   return (
-  <h1> Hello World </h1>
+  <div>
+      <Labs />
+      <h1> Hello World </h1>
+  </div>
   );
 }
 export default App;
