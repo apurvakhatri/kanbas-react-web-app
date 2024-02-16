4975523c14e19277415e68038f91beec28d5f8e1
apurvakhatri <apurvakhatri2011@gmail.com>
2024-02-16 12:49:15 -0500

    Router added

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import logo from './logo.svg';
 import Labs from "./Labs";
 import HelloWorld from './Labs/a3/HelloWorld';
 import Kanbas from "./Kanbas";
+import {HashRouter} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router";
 
 //import './App.css';
 
 function App() {
   return (
-  <div>
-      < Labs />
-      < Kanbas />
-      < HelloWorld />
-  </div>
+  <HashRouter>
+      <div>
+        <Routes>
+            <Route path="/Labs/*" element={<Labs/>}/>
+            <Route path="/Kanbas/*" element={<Kanbas/>}/>
+            <Route path="/Labs/a3/*" element={<HelloWorld/>}/>
+        </Routes>
+      </div>
+  </HashRouter>
   );
 }
 export default App;
