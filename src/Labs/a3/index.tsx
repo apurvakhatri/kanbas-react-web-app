7beb705927dbc88a8be750ea380b426e9cfab331
apurvakhatri <apurvakhatri2011@gmail.com>
2024-02-17 13:38:58 -0500

    2.4 added

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,11 +1,12 @@
 import JavaScript from "./JavaScript";
 import PathParameters from "./routing/PathParameters"
-
+import Classes from "./Classes";
 
 function Assignment3() {
     return (
         <div className="container">
             <h1>Assignment 3</h1>
+            <Classes />
             <PathParameters />
             <JavaScript/>
         </div>
