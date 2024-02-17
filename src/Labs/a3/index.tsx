c5613dd3c514798749b22794113dceac669a4a14
apurvakhatri <apurvakhatri2011@gmail.com>
2024-02-17 14:45:49 -0500

    Styles added

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,11 +1,13 @@
 import JavaScript from "./JavaScript";
 import PathParameters from "./routing/PathParameters"
 import Classes from "./Classes";
+import Styles from "./Styles";
 
 function Assignment3() {
     return (
         <div className="container">
             <h1>Assignment 3</h1>
+            <Styles />
             <Classes />
             <PathParameters />
             <JavaScript/>
