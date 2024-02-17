93f2f7f7df48a68049999190a4fc95e5a89e4b17
apurvakhatri <apurvakhatri2011@gmail.com>
2024-02-17 15:37:19 -0500

    Child component added

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -3,11 +3,16 @@ import PathParameters from "./routing/PathParameters"
 import Classes from "./Classes";
 import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
+import Highlight from "./Highlight";
 
 function Assignment3() {
     return (
         <div className="container">
             <h1>Assignment 3</h1>
+            <Highlight>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+                vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+            </Highlight>
             <ConditionalOutput />
             <Styles />
             <Classes />
