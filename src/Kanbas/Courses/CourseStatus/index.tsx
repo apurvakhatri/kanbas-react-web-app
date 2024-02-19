55e73d91cdc1ea36197a23e7c8261d0a644328d8
apurvakhatri <apurvakhatri2011@gmail.com>
2024-02-19 11:21:36 -0500

    Home screen added

---
+++ src/Kanbas/Courses/CourseStatus/index.tsx
@@ -0,0 +1,22 @@
+import "./index.css";
+
+function CourseStatus() {
+  return (
+    <div>
+      <button className="btn wd-bg-lightgray mt-2">Import Existing Content</button>
+      <br />
+      <button className="btn wd-bg-lightgray mt-2">Import from Commons</button>
+      <br />
+      <button className="btn wd-bg-lightgray mt-2">Choose Home Page</button>
+      <br />
+      <button className="btn wd-bg-lightgray mt-2">View Course Stream</button>
+      <br />
+      <button className="btn wd-bg-lightgray mt-2">New Announcement</button>
+      <br />
+      <button className="btn wd-bg-lightgray mt-2">New Analytics</button>
+      <br />
+    </div>
+  );
+}
+
+export default CourseStatus;
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import { HiMiniBars3 } from "react-icons/hi2";
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
+import Home from "./Home";
 
 function Courses() {
   const { courseId } = useParams();
@@ -16,7 +17,7 @@ function Courses() {
           <div className="overflow-y-scroll position-fixed bottom-0 end-0" style={{ left: "320px", top: "50px" }} >
               <Routes>
                   <Route path="/" element={<Navigate to="Home" />} />
-                  <Route path="Home" element={<h1>Home</h1>} />
+                 <Route path="Home" element={ <Home />} />
                   <Route path="Modules" element={<Modules/>} />
 {/*                   <Route path="Modules" element={<h1>Modules</h1>} /> */}
                   <Route path="Piazza" element={<h1>Piazza</h1>} />
