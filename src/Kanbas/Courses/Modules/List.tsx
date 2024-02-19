dea02c542e20278cf2b3a30e09ba5deb8ad7bd16
apurvakhatri <apurvakhatri2011@gmail.com>
2024-02-19 17:10:37 -0500

    react-icons added

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { modules } from "../../Database";
 import './index.css';
 import ModuleButtons from "./ModuleButtons";
+import { AiOutlinePlus, AiFillCheckCircle } from "react-icons/ai";
+import { HiOutlineEllipsisVertical } from "react-icons/hi2";
 
 function List() {
   const { courseId } = useParams();
@@ -17,9 +19,16 @@ function List() {
          .filter((module) => module.course === courseId)
          .map((module, index) => (
            <li key={index} className="list-group-item wd-module-item mb-4">
-             <h5>{module.name}</h5>
-             <p>{module.description}</p>
-
+             <div className="module-content">
+              <div>
+                <h5>{module.name}</h5>
+                <p>{module.description}</p>
+              </div>
+              <div className="icons">
+                <AiFillCheckCircle color="green" />
+                <HiOutlineEllipsisVertical className="ellipsis-icon"/>
+              </div>
+            </div>
            </li>
       ))
     }
