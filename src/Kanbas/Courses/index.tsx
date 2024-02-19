988085acadbe518430d1ff6f12600117b185a9b2
apurvakhatri <apurvakhatri2011@gmail.com>
2024-02-19 11:31:26 -0500

    Assignments added

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -5,6 +5,8 @@ import { HiMiniBars3 } from "react-icons/hi2";
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from "./Assignments";
+
 
 function Courses() {
   const { courseId } = useParams();
@@ -21,7 +23,8 @@ function Courses() {
                   <Route path="Modules" element={<Modules/>} />
 {/*                   <Route path="Modules" element={<h1>Modules</h1>} /> */}
                   <Route path="Piazza" element={<h1>Piazza</h1>} />
-                  <Route path="Assignments" element={<h1>Assignments</h1>} />
+                  <Route path="Assignments" element={<Assignments/>} />
+{/*                   <Route path="Assignments" element={<h1>Assignments</h1>} /> */}
                   <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
                   <Route path="Grades" element={<h1>Grades</h1>} />
               </Routes>
