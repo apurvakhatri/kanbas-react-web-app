8b5e3c62a6fd58ce526fdb4fd91c867120c08530
apurvakhatri <apurvakhatri2011@gmail.com>
2024-03-03 16:51:35 -0500

    2.3 Managing Component State

---
+++ src/Labs/a4/Counter.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+function Counter() {
+//  let count = 7;
+  const [count, setCount] = useState(7);
+  console.log(count);
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button
+        onClick={() => {
+            //count++;
+            setCount(count + 1);
+            console.log(count);
+        }}>
+        Up
+      </button>
+      <button
+        onClick={() => {
+            //count--;
+            setCount(count + 1);
+            console.log(count);
+        }}>
+        Down
+      </button>
+    </div>
+); }
+export default Counter;
