732496477d0500ba2739b2d4ae7298c2dd2ba563
apurvakhatri <apurvakhatri2011@gmail.com>
2024-03-04 12:56:45 -0500

    2.4 - Working with Redux components

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -10,6 +10,7 @@ import DateStateVariable from "./DateStateVariable";
 import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
 const Assignment4 = () => {
     function sayHello() {
@@ -29,6 +30,7 @@ const Assignment4 = () => {
         <ObjectStateVariable />
         <ArrayStateVariable />
         <ParentStateComponent />
+        <ReduxExamples />
     </>
     );
 };
